eaae0846ec4a22f7a9f64e3ce52d78cc1a98de6e
Mike Lysikov <d8corp@mail.ru>
2022-11-23 15:51:55 +0300

    v0.9.4

--- release/types.d.ts
+++ release/types.d.ts
@@ -3,12 +3,12 @@ import { Ref } from './utils';
 export type ContentElements = TargetElements | Text;
 export type TargetElements = Element | Comment;
 export type UseComment = [Handler, Comment];
+export type WatchProp<T> = T | ((update: boolean) => T);
 export type HTMLProps<E extends HTMLElement = HTMLElement, K extends keyof E = keyof E> = Partial<Record<`${'_' | '$'}${Exclude<K, symbol | `on${string}`>}`, any>> & Partial<Record<Extract<K, `on${string}`>, (e: Event) => void>> & {
-    style?: string;
+    style?: WatchProp<string>;
     ref?: Ref<E>;
     [prop: string]: any;
 };
-export type WatchProp<T> = T | ((update: boolean) => T);
 declare global {
     interface Comment {
         _children: ContentElements[];
